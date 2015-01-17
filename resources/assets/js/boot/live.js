d3a961c0cc6331b2fb8d4883e5003bf4b9da82cf
t2t2 <taavot@gmail.com>
2015-01-17 06:58:01 +0200

    Live page design, time to get the code on

---
+++ resources/assets/js/boot/live.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+require('../../../../bower_components/foundation/js/foundation/foundation');
+require('../../../../bower_components/foundation/js/foundation/foundation.topbar');
+
+$(function () {
+	$(document).foundation();
+});
--- resources/views/live/home.blade.php
+++ resources/views/live/home.blade.php
@@ -1,10 +1,46 @@
 <!doctype html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Live | {{ config('livehub.brand') }}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{ config('livehub.brand') }}</title>
+
+	<link rel="stylesheet" href="{{ asset('assets/css/live.css') }}"/>
+
+	<script src="{{ asset('assets/js/head.js') }}"></script>
 </head>
 <body>
-	
+	<div id="container">
+		<div id="header">
+			<nav class="top-bar" data-topbar role="navigation" data-options="mobile_show_parent_link: false">
+				<ul class="title-area">
+					<li class="name">
+						<h1><a href="#">{{ config('livehub.brand') }}</a></h1>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span class="live-streams-text">Now Live Streams: 0</span></a></li>
+				</ul>
+				<section class="top-bar-section">
+					<ul class="left">
+						<li class="has-dropdown">
+							<a href="#">
+								<span class="show-for-medium-up live-streams-text">Now Live Streams: 0</span>
+								<span class="show-for-small-only">Switch Stream</span>
+							</a>
+							<ul class="dropdown" id="streams-list">
+							</ul>
+						</li>
+					</ul>
+				</section>
+			</nav>
+		</div>
+		<div id="live-container">
+			<iframe src="about:blank" frameborder="0"></iframe>
+		</div>
+		<div id="chat-container">
+			<iframe src="about:blank" frameborder="0"></iframe>
+		</div>
+	</div>
+
+	<script src="{{ asset('assets/js/live.js') }}"></script>
 </body>
 </html>
